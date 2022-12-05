f7e825a6fd5f923e06fe5dfd6aca688bbc690239
YiPing Pan <pan.yiping.fi@gmail.com>
2022-12-05 22:57:50 +0800

    Solve question 543

--- Tree/543_Diameter_of_Binary_Tree.c
+++ Tree/543_Diameter_of_Binary_Tree.c
@@ -1,4 +1,6 @@
 /* https://leetcode.com/problems/diameter-of-binary-tree/ */
+#define NULL 0
+#define max(x, y) (x) > (y) ? (x) : (y)
 
 /* Definition for a binary tree node. */
 struct TreeNode {
@@ -7,8 +9,22 @@ struct TreeNode {
   struct TreeNode *right;
 };
 
-int diameterOfBinaryTree(struct TreeNode *root) {
-  if (!root) {return 0;}
+int diameterOfBinaryTreeHelper(struct TreeNode *root, int *maxAns) {
+  if (root == NULL) { return 0; }
 
-  return 0;
+  int hL = diameterOfBinaryTreeHelper(root->left, maxAns);
+  int hR = diameterOfBinaryTreeHelper(root->right, maxAns);
+
+  if (*maxAns < hL + hR) {
+    *maxAns = hL + hR;
+  }
+
+  return (hL > hR ? hL : hR) + 1;
+}
+
+int diameterOfBinaryTree(struct TreeNode *root) {
+  int max = 0;
+  diameterOfBinaryTreeHelper(root, &max);
+
+  return max;
 }
