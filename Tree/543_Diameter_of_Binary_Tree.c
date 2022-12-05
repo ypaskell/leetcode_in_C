9e0aa626057125d461ff5a56ce6dbc465c7588ba
YiPing Pan <pan.yiping.fi@gmail.com>
2022-12-05 22:33:38 +0800

    New practice unsolved

---
+++ Tree/543_Diameter_of_Binary_Tree.c
@@ -0,0 +1,14 @@
+/* https://leetcode.com/problems/diameter-of-binary-tree/ */
+
+/* Definition for a binary tree node. */
+struct TreeNode {
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+
+int diameterOfBinaryTree(struct TreeNode *root) {
+  if (!root) {return 0;}
+
+  return 0;
+}
